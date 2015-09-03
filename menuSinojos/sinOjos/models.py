686f804a1506dec51eb3d5d267827bb03da65f0c
Jesus Gonzalez <gkz2024@gmail.com>
2015-09-03 17:42:49 +0200

    Primeras clases (tablas) en el modelo

--- menuSinojos/sinOjos/models.py
+++ menuSinojos/sinOjos/models.py
@@ -1,4 +1,22 @@
 ﻿from django.db import models
 
-# Create your models here.
-#Aqui va nuestro modelo
+#JesusGonzalez_20150903: Creamos las primeras clases del model
+#doc e info: https://docs.djangoproject.com/en/1.8/topics/db/models/
+
+class Restaurantes(models.Model):
+    restaurante_id = models.IntegerField(primary_key=True) #Clave primaria explicita
+    nombre =  models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    mapa = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    codigoPostal = models.IntegerField(null=True)
+    baja_logica = models.SmallIntegerField(null=True)
+    fecha_alta = models.DateTimeField()
+    fecha_baja = models.DateTimeField(null=True)
+
+class Restaurantes_fotos(models.Model): 
+    restaurante_foto_id = models.IntegerField(primary_key=True) #Clave primaria explicita
+    restaurante = models.ForeignKey(Restaurantes) #clave foranea
+    nombre = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
